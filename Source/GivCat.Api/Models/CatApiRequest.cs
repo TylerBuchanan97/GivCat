c4a4b3e194579be256504c81f8b4cfd374cabfa8
Tyler Buchanan <t.buchananx97@gmail.com>
2019-01-21 18:24:46 -0500

    Added request model for use with the Cat Api.

---
+++ Source/GivCat.Api/Models/CatApiRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace GivCat.Api.Models
+{
+    public class CatApiRequest
+    {
+        public string BaseRequestUrl => "https://api.thecatapi.com/v1/images/search";
+
+        public string Breed { get; set; }
+
+        public string Category { get; set; }
+    }
+}
