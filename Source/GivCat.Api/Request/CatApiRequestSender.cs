22a9bcb7804b1c7bae2db21a2d3ee3a1de157583
Tyler Buchanan <t.buchananx97@gmail.com>
2019-01-22 18:56:40 -0500

    Modified response handling to deserialize the object into a CatApiResponse.

--- Source/GivCat.Api/Request/CatApiRequestSender.cs
+++ Source/GivCat.Api/Request/CatApiRequestSender.cs
@@ -1,24 +1,25 @@
 ﻿namespace GivCat.Api.Request
 {
+    using System.Linq;
     using System.Net.Http;
     using System.Threading.Tasks;
 
     using GivCat.Api.Common;
     using GivCat.Api.Models;
 
-    using Newtonsoft.Json.Linq;
+    using Newtonsoft.Json;
 
     public class CatApiRequestSender : IRequestSender<CatApiRequest, CatApiResponse>
     {
         public async Task<CatApiResponse> SendRequestAsync(CatApiRequest request)
         {
-            HttpResponseMessage response = await new HttpClient().GetAsync(request.BaseRequestUrl);
+            HttpResponseMessage rawResponse = await new HttpClient().GetAsync(request.BaseRequestUrl);
 
-            JArray responseContent = JArray.Parse(await response.Content.ReadAsStringAsync());
+            string responseAsString = await rawResponse.Content.ReadAsStringAsync();
 
-            string imageUrl = responseContent?.First["url"]?.Value<string>();
-
-            return !string.IsNullOrWhiteSpace(imageUrl) ? new CatApiResponse { Url = imageUrl } : null;
+            return string.IsNullOrWhiteSpace(responseAsString)
+                       ? null
+                       : JsonConvert.DeserializeObject<CatApiResponse[]>(responseAsString).FirstOrDefault();
         }
     }
 }
