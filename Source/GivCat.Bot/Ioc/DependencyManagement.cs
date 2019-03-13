e4e92b98a40b8c20498f22b28d2e0d93a8f71bf9
Tyler Buchanan <t.buchananx97@gmail.com>
2019-03-12 21:42:53 -0400

    Added DependencyManagement class. Fixed issue where wrong solution file was being used.

---
+++ Source/GivCat.Bot/Ioc/DependencyManagement.cs
@@ -0,0 +1,39 @@
+﻿namespace GivCat.Bot.Ioc
+{
+    using System;
+
+    using Discord;
+    using Discord.Commands;
+    using Discord.WebSocket;
+
+    using GivCat.Bot.Commands;
+    using GivCat.Bot.Initialization;
+
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class DependencyManagement
+    {
+        private static IServiceProvider provider;
+
+        public static IServiceProvider Provider => provider ?? (provider = CreateServiceProvider());
+
+        private static IConfiguration CreateConfiguration()
+        {
+            string environment = Environment.GetEnvironmentVariable("ENVIRONMENT");
+
+            return new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", false, true).AddJsonFile($"appsettings.{environment}.json", true, true)
+                .Build();
+        }
+
+        private static IServiceProvider CreateServiceProvider()
+        {
+            return new ServiceCollection().AddTransient<IBotInitializer, BotInitializer>()
+                .AddTransient<ICommandProcessor, CommandProcessor>().AddSingleton(CreateConfiguration())
+                .AddSingleton(new CommandService())
+                .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig { LogLevel = LogSeverity.Info }))
+                .BuildServiceProvider();
+        }
+    }
+}
--- Source/GivCat.Bot/Program.cs
+++ Source/GivCat.Bot/Program.cs
@@ -3,12 +3,8 @@
     using System;
     using System.Threading.Tasks;
 
-    using Discord;
-    using Discord.Commands;
-    using Discord.WebSocket;
-
-    using GivCat.Bot.Commands;
     using GivCat.Bot.Initialization;
+    using GivCat.Bot.Ioc;
 
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
@@ -17,31 +13,11 @@
     {
         public static async Task Main()
         {
-            IConfiguration configuration = CreateConfiguration();
-
-            IServiceProvider serviceProvider = CreateServiceProvider(configuration);
+            IServiceProvider serviceProvider = DependencyManagement.Provider;
 
             await serviceProvider.GetService<IBotInitializer>().InitializeGivCatBot();
 
             await Task.Delay(-1);
-        }
-
-        private static IConfiguration CreateConfiguration()
-        {
-            string environment = Environment.GetEnvironmentVariable("ENVIRONMENT");
-
-            return new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory)
-                .AddJsonFile("appsettings.json", false, true).AddJsonFile($"appsettings.{environment}.json", true, true)
-                .Build();
-        }
-
-        private static ServiceProvider CreateServiceProvider(IConfiguration configuration)
-        {
-            return new ServiceCollection().AddTransient<IBotInitializer, BotInitializer>()
-                .AddTransient<ICommandProcessor, CommandProcessor>().AddSingleton(configuration)
-                .AddSingleton(new CommandService())
-                .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig { LogLevel = LogSeverity.Info }))
-                .BuildServiceProvider();
         }
     }
 }
